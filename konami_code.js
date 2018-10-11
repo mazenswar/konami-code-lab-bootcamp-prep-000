2b2c08451aed0631cf89e415844e39caf1af4f03
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-11 22:14:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  document.addEventListener('keydown', function(event) {
+    let index = 0;
+    for(let i = 0; i <= codes.length; i++) {
+      if(event.code === codes[i]) {
+        index++; 
+        if(index.length === codes.length) {
+            alert("You did it!");
+          }
+        
+      }
+      else{
+        index = 0;}
+    }
+  });
 }
